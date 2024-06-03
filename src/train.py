c983a77e8ac9262401020bdcf765d420093f71d3
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-06-03 11:37:50 +0200

    Simplified training script

--- src/train.py
+++ src/train.py
@@ -2,9 +2,6 @@ import logging
 import sys
 
 import wandb
-from imitation.rewards.reward_nets import (
-    ShapedRewardNet, cnn_transpose,
-)
 from imitation.rewards.reward_wrapper import RewardVecEnvWrapper
 from stable_baselines3.common.base_class import SelfBaseAlgorithm
 from stable_baselines3.common.evaluation import evaluate_policy
@@ -29,16 +26,16 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 def train_agent(agent: SelfBaseAlgorithm, agent_name, num_experiment=0):
     logging.info(f"Training {agent_name}")
 
-    model_path = f"{environment_dir}{CONFIG.models_dir}{agent_name}/{num_experiment}/{CONFIG.ppo_train.env_steps}"
+    model_path = f"{environment_dir}{CONFIG.models_dir}{agent_name}/{num_experiment}/{CONFIG.ppo[env_id].env_steps}"
 
     # After every 10000 training steps, we evaluate the agent
     mean_reward, std_reward = evaluate_policy(agent.policy, eval_venv, n_eval_episodes=8)
     wandb.log({f"eval_mean_reward": mean_reward, f"eval_std_reward": std_reward})
 
-    for j in range(1, CONFIG.ppo_train.iterations):  # set to 1_000_000 time steps in total for better performance
+    for j in range(1, CONFIG.ppo[env_id].iterations):  # set to 1_000_000 time steps in total for better performance
         logging.info(f"Training {agent_name} iteration {j}")
 
-        agent.learn(total_timesteps=CONFIG.ppo_train.env_steps, reset_num_timesteps=False, tb_log_name=agent_name,
+        agent.learn(total_timesteps=CONFIG.ppo[env_id].env_steps, reset_num_timesteps=False, tb_log_name=agent_name,
                     callback=WandbCallback(
                         gradient_save_freq=100,
                         model_save_freq=0,  # we do not save the model while training,
@@ -50,7 +47,7 @@ def train_agent(agent: SelfBaseAlgorithm, agent_name, num_experiment=0):
         logging.info(f"Evaluating {agent_name} iteration {j}")
 
         # After every 10000 training steps, we evaluate the agent
-        mean_reward, std_reward = evaluate_policy(agent.policy, eval_venv, n_eval_episodes=8)
+        mean_reward, std_reward = evaluate_policy(agent.policy, eval_venv, n_eval_episodes=10)
         wandb.log({f"eval_mean_reward": mean_reward, f"eval_std_reward": std_reward})
 
     agent.save(model_path)
@@ -79,24 +76,7 @@ def train_rlhf(env, env_id, agent, seed, tensorboard_log, query_strategy, confli
                                         tensorboard_log=tensorboard_log)
 
 
-def get_reward(env, reward_net, env_id, agent):
-    if env_id == 'SpaceInvaders-v4':
-        def value_potential(state):
-            state_ = cnn_transpose(state)
-            return agent.policy.predict_values(state_)
-
-        shaped_reward_net = ShapedRewardNet(
-            base=reward_net,
-            potential=value_potential,
-            discount_factor=0.99,
-        )
-
-        return RewardVecEnvWrapper(venv, shaped_reward_net.predict_processed)
-    else:
-        return RewardVecEnvWrapper(env, reward_net)
-
-
-# Run the experiments#
+# Run the experiments
 for query_strategy in CONFIG.QUERY_STRATEGIES:
     for env_id in CONFIG.ENVIRONMENTS:
         for i in range(CONFIG.num_experiments):
@@ -107,16 +87,15 @@ for query_strategy in CONFIG.QUERY_STRATEGIES:
             venv = src.environments.get_environment(env_id, CONFIG.env.training_n_envs, seed=seed)
             # We use a separate environment for evaluation
             eval_venv = src.environments.get_environment(env_id, CONFIG.env.eval_n_envs, seed=seed)
-            # We update the random number generators of PyTorch, Numpy,
 
+            # We set up the environment directory and the log directory
             environment_dir = f"{env_id}/"
-
             tensorboard_log = environment_dir + CONFIG.logdir
 
             # Hyperparameters to log in wandb
             wandb_config = {
                 "policy_type": "MlpPolicy",
-                "total_timesteps": CONFIG.ppo_train.env_steps * CONFIG.ppo_train.iterations,
+                "total_timesteps": CONFIG.ppo[env_id].env_steps * CONFIG.ppo[env_id].iterations,
                 "env_name": env_id,
             }
 
@@ -135,125 +114,31 @@ for query_strategy in CONFIG.QUERY_STRATEGIES:
 
             run.finish()
 
-            (reward_net_0, main_trainer_0, agent_trainer_0, results_0) = train_rlhf(venv, env_id, None, seed,
-                                                                                    tensorboard_log, query_strategy,
-                                                                                    conflicting_prob=0.0)
+            # RLHF agents with their corresponding diversity levels
+            data = [["learner_0", 0.0], ["learner_25", 0.25], ["learner_40", 0.40],
+                    ["learner_50", 0.50], ["learner_75", 0.75], ["learner_100", 1.0]]
 
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_0_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
+            for learner_id, conflicting_prob in data:
+                (reward_net, main_trainer, agent_trainer, results) = train_rlhf(venv, env_id, None, seed,
+                                                                                tensorboard_log, query_strategy,
+                                                                                conflicting_prob=conflicting_prob)
 
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_0 = get_reward(venv, reward_net_0.predict_processed, env_id, agent_trainer_0)
-            learner_0 = initialize_agent(learned_reward_venv_0, seed, tensorboard_log, env_id)
-            train_agent(learner_0, "learner_0", i)
+                run = wandb.init(
+                    project=env_id,
+                    name=f"{learner_id}_{i}_{query_strategy}",
+                    config=wandb_config,
+                    sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
+                    monitor_gym=False,  # auto-upload the videos of agents playing the game
+                    save_code=False,  # optional
+                )
 
-            run.finish()
+                # We train an agent that sees only the shaped, learned reward
+                learned_reward_venv = RewardVecEnvWrapper(venv, reward_net.predict_processed)
 
-            (reward_net_25, main_trainer_25, agent_trainer_25, results_25) = train_rlhf(venv, env_id, None, seed,
-                                                                                        tensorboard_log, query_strategy,
-                                                                                        conflicting_prob=0.25)
+                learner = initialize_agent(learned_reward_venv, seed, tensorboard_log, env_id)
+                train_agent(learner, learner_id, i)
 
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_25_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
+                run.finish()
 
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_25 = get_reward(venv, reward_net_25.predict_processed, env_id, agent_trainer_25)
-            learner_25 = initialize_agent(learned_reward_venv_25, seed, tensorboard_log, env_id)
-            train_agent(learner_25, "learner_25", i)
-
-            run.finish()
-
-            (reward_net_40, main_trainer_40, agent_trainer_40, results_40) = train_rlhf(venv, env_id, None, seed,
-                                                                                        tensorboard_log, query_strategy,
-                                                                                        conflicting_prob=0.40)
-
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_40_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
-
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_40 = get_reward(venv, reward_net_40.predict_processed, env_id, agent_trainer_40)
-            learner_40 = initialize_agent(learned_reward_venv_40, seed, tensorboard_log, env_id)
-            train_agent(learner_40, "learner_40", i)
-
-            run.finish()
-
-            (reward_net_50, main_trainer_50, agent_trainer_50, results_50) = train_rlhf(venv, env_id, None, seed,
-                                                                                        tensorboard_log, query_strategy,
-                                                                                        conflicting_prob=0.50)
-
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_50_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
-
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_50 = get_reward(venv, reward_net_50.predict_processed, env_id, agent_trainer_50)
-            learner_50 = initialize_agent(learned_reward_venv_50, seed, tensorboard_log, env_id)
-            train_agent(learner_50, "learner_50", i)
-
-            run.finish()
-
-            (reward_net_75, main_trainer_75, agent_trainer_75, results_75) = train_rlhf(venv, env_id, None, seed,
-                                                                                        tensorboard_log, query_strategy,
-                                                                                        conflicting_prob=0.75)
-
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_75_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
-
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_75 = get_reward(venv, reward_net_75.predict_processed, env_id, agent_trainer_75)
-            learner_75 = initialize_agent(learned_reward_venv_75, seed, tensorboard_log, env_id)
-            train_agent(learner_75, "learner_75", i)
-
-            run.finish()
-
-            (reward_net_100, main_trainer_100, agent_trainer_100, results_100) = train_rlhf(venv, env_id, None,
-                                                                                            seed, tensorboard_log,
-                                                                                            query_strategy,
-                                                                                            conflicting_prob=1.0)
-
-            run = wandb.init(
-                project=env_id,
-                name=f"learner_100_{i}_{query_strategy}",
-                config=wandb_config,
-                sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-                monitor_gym=False,  # auto-upload the videos of agents playing the game
-                save_code=False,  # optional
-            )
-
-            # We train an agent that sees only the shaped, learned reward
-            learned_reward_venv_100 = get_reward(venv, reward_net_100.predict_processed, env_id, agent_trainer_100)
-            learner_100 = initialize_agent(learned_reward_venv_100, seed, tensorboard_log, env_id)
-            train_agent(learner_100, "learner_100", i)
-
-            run.finish()
             venv.close()
             eval_venv.close()
