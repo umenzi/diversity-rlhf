54e0c265cf0c7df2c2e85a71fdbbb42fff50331d
Javier Paez Franco <j.paezfranco@student.tudelft.nl>
2024-06-07 14:59:54 +0200

    Updated hyperparameters

--- src/Config.py
+++ src/Config.py
@@ -9,19 +9,8 @@ from stable_baselines3.ppo import MlpPolicy, CnnPolicy
 class EnvConfig:
     vectorized: bool = True
     render: bool = False
-    training_n_envs: int = 2
+    training_n_envs: int = 8
     eval_n_envs: int = 1
-
-
-@dataclass
-class PPOTrainConfig:
-    """
-    Config for training the expert PPO
-    """
-
-    # We train agents for a total of 1_000_000 steps
-    env_steps: int = 10_000  # we evaluate PPO after this many steps
-    iterations: int = 100
 
 
 @dataclass
@@ -31,54 +20,67 @@ class PPOConfig:
 
     Hyperparameters obtained with Optuna, initially inspired by: https://huggingface.co/sb3/ppo-LunarLander-v2
     """
+    # We train agents for a total of 1_000_000 steps
+    env_steps: int = 10_000  # we evaluate PPO after this many steps
+    iterations: int = 100
 
     batch_size = 64
     clip_range = 0.1
-    ent_coef = 0.01
+    ent_coef = 0.0
     learning_rate = 0.0003
     gamma = 0.999
-    gae_lambda = 0.9
-    n_epochs = 4
+    gae_lambda = 0.99
+    n_epochs = 10
     n_steps = 2048
+    use_sde = False
+    sde_sample_freq = -1
+
+
+@dataclass
+class PendulumPPOConfig(PPOConfig):
+    # specific parameters for the Pendulum environment
+    # https://huggingface.co/sb3/ppo-Pendulum-v1
+    policy = MlpPolicy
+    env_steps = 1_000
+    clip_range = 0.2
+    ent_coef = 0.01
+    learning_rate = 0.001
+    gae_lambda = 0.95
+    gamma = 0.91
+    n_epochs = 10
+    n_steps = 1024
+    use_sde = True
+    sde_sample_freq = 4
 
 
 @dataclass
 class LunarPPOConfig(PPOConfig):
     # specific parameters for the Lunar Lander environment
+    # https://huggingface.co/sb3/ppo-LunarLander-v2
     policy = MlpPolicy
-
-
-@dataclass
-class SpacePPOConfig(PPOConfig):
-    # specific parameters for the Space Invaders environment
-    policy = CnnPolicy
-    batch_size = 256
-    clip_range = 0.3
-    ent_coef = 0.093
-    learning_rate = 1.9e-05
-    gae_lambda = 0.96
-    gamma = 0.93
+    ent_coef = 0.01
     n_epochs = 4
-    n_steps = 128
+    gae_lambda = 0.9
 
 
 @dataclass
-class AntPPOConfig(PPOConfig):
-    # specific parameters for the Ant Environment
+class WalkerPPOConfig(PPOConfig):
+    # specific parameters for the Bipedal Walker Environment
+    # https://huggingface.co/sb3/ppo-BipedalWalker-v3
     policy = MlpPolicy
-    batch_size = 256
-    clip_range = 0.2
-    ent_coef = 0.038
-    learning_rate = 7.55e-05
-    gae_lambda = 0.88
-    gamma = 0.95
+    env_steps = 20_000
+    batch_size = 64
+    clip_range = 0.18
+    learning_rate = 0.0003
+    gae_lambda = 0.95
+    gamma = 0.999
     n_epochs = 10
-    n_steps = 128
+    n_steps = 2048
 
 
 @dataclass
 class RLHFConfig:
-    total_timesteps = 200_000
+    total_timesteps = 500_000
     total_comparisons = 500
     num_iterations = 60
 
@@ -94,6 +96,16 @@ class RLHFConfig:
 
 
 @dataclass
+class PendulumRLHFConfig(RLHFConfig):
+    # specific parameters for the Pendulum environment
+    transition_oversampling = 1.3
+    initial_comparison_frac = 0.2
+    exploration_frac = 0.06
+    temperature = 0.22
+    discount_factor = 1
+
+
+@dataclass
 class LunarRLHFConfig(RLHFConfig):
     # specific parameters for the Lunar Lander environment
     fragment_length = 97
@@ -105,45 +117,31 @@ class LunarRLHFConfig(RLHFConfig):
 
 
 @dataclass
-class SpaceRLHFConfig(RLHFConfig):
-    # specific parameters for the Space Invaders environment
-    fragment_length = 116
-    transition_oversampling = 1.69
-    initial_comparison_frac = 0.29
-    exploration_frac = 0.06
-    temperature = 0.22
+class WalkerRLHFConfig(RLHFConfig):
+    # specific parameters for the Bipedal Walker Environment
+    total_timesteps = 2_000_000
+    total_comparisons = 700
+
+    fragment_length = 100
+    transition_oversampling = 1.7
+    initial_comparison_frac = 0.32
+    exploration_frac = 0.25
+    temperature = 1.8
     discount_factor = 0.96
 
 
 @dataclass
-class AntRLHFConfig(RLHFConfig):
-    total_timesteps = 2_000_000
-    total_comparisons = 1_000
-    num_iterations = 100
-    initial_epoch_multiplier = 6
-
-    # specific parameters for the Ant Environment
-    fragment_length = 1_000
-    transition_oversampling = 1.5
-    initial_comparison_frac = 0.68
-    exploration_frac = 0.25
-    temperature = 0.84
-    discount_factor = 0.99
-
-
-@dataclass
 class Config:
-    ENVIRONMENTS = ["LunarLander-v2", "SpaceInvaders-v4", 'Ant-v1']
+    ENVIRONMENTS = ["Pendulum-v1", "LunarLander-v2", "BipedalWalker-v3"]
     QUERY_STRATEGIES = ["random", "active"]
 
     env: EnvConfig = field(default_factory=EnvConfig)
-    ppo_train: PPOTrainConfig = field(default_factory=PPOTrainConfig)
-    ppo: Dict[str, Union[LunarPPOConfig, SpacePPOConfig, AntPPOConfig]] = field(
-        default_factory=lambda: {'LunarLander-v2': LunarPPOConfig(), 'SpaceInvaders-v4': SpacePPOConfig(),
-                                 'Ant-v1': AntPPOConfig})
-    rlhf: Dict[str, Union[LunarRLHFConfig, SpaceRLHFConfig, AntRLHFConfig]] = field(
-        default_factory=lambda: {'LunarLander-v2': LunarRLHFConfig(), 'SpaceInvaders-v4': SpaceRLHFConfig(),
-                                 'Ant-v1': AntRLHFConfig})
+    ppo: Dict[str, Union[PendulumPPOConfig, LunarPPOConfig, WalkerPPOConfig]] = field(
+        default_factory=lambda: {"Pendulum-v1": PendulumPPOConfig(), "LunarLander-v2": LunarPPOConfig(),
+                                 "BipedalWalker-v3": WalkerPPOConfig})
+    rlhf: Dict[str, Union[PendulumRLHFConfig, LunarRLHFConfig, WalkerRLHFConfig]] = field(
+        default_factory=lambda: {"Pendulum-v1": PendulumRLHFConfig(), "LunarLander-v2": LunarRLHFConfig(),
+                                 "BipedalWalker-v3": WalkerRLHFConfig})
 
     device = th.device("cuda" if th.cuda.is_available() else "cpu")
 
